d38bbd1c30534c49137456fa9a0ad9ec7416bd80
Kimberly Mireya Elias Diaz <kim.eliasdiaz@gmail.com>
2022-02-02 18:22:39 -0600

    Navigation in children

--- genealogy-tree/src/components/node/Node.js
+++ genealogy-tree/src/components/node/Node.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import './Node.css';
 
 const Node = (attributes) => {
@@ -9,34 +9,75 @@ const Node = (attributes) => {
   return (
     <div>
         <div className="card-avatar">
-            <img className="img" src="https://i.pinimg.com/originals/d5/b0/4c/d5b04cc3dcd8c17702549ebc5f1acf1a.png"></img>
+            <img className="img" src={require("../../assets/person.png")}></img>
         </div>
         <h6 className="category">{node.username}</h6>
+        <div className="title">
+            <table>
+                <tr>
+                    <th>Full name</th>
+                    <td>{node.full_name}</td>
+                </tr>
+                <tr>
+                    <th>Status</th>
+                    <td>{node.status}</td>
+                </tr>
+                <tr>
+                    <th>Product</th>
+                    <td>{node.product_name}</td>
+                </tr>
+                <tr>
+                    <th>Category</th>
+                    <td>{node.category_name}</td>
+                </tr>
+            </table>
+        </div>
         <ChildrenNode children={node.children} level={level}/>
-        
     </div>
   );
 }
 
+
+
 const ChildrenNode = (attributes) => {
     const children = attributes.children
+
+    const show_hide_1 = (id) => {
+        const element = document.getElementById(id)
+        element.style.display = 'grid'
+
+        const elementButton = document.getElementById('button-'+id)
+        elementButton.style.display = 'none'
+    }
+
     if(children != undefined) {
         const level = attributes.level
+        const id_parent = children[0].parent_id
     
         if(children.length == 1) {
-            if(level < 4) {
+            if(level < 3) {
                 return (
-                    <div className="Node-item-1">
+                    <div id={id_parent} className="Node-item-1">
                         <div>
                             <Node data={children[0]} level={level}/>
                         </div>
                     </div>
                 );
-            } else {
+            } else if(level == 3) {
                 return (
-                    <div style={{display:"none"}} className="Node-item-1">
+                    <div id={id_parent} className="Node-item-1">
                         <div>
                             <Node data={children[0]} level={level}/>
+                            <button id={'button-'+children[0].distributor_id} onClick={() => {show_hide_1(children[0].distributor_id)}}>+</button>
+                        </div>
+                    </div>
+                );
+            } else {
+                return (
+                    <div id={id_parent} style={{display:"none"}} className="Node-item-1">
+                        <div>
+                            <Node data={children[0]} level={level}/>
+                            <button id={'button-'+children[0].distributor_id} onClick={() => {show_hide_1(children[0].distributor_id)}}>+</button>
                         </div>
                     </div>
                 );
@@ -53,9 +94,9 @@ const ChildrenNode = (attributes) => {
                 placement.right = children[0]
             }
 
-            if(level < 4) {
+            if(level < 3) {
                 return (
-                    <div className="Node-item-2">
+                    <div id={id_parent} className="Node-item-2">
                         <div>
                             <Node data={placement.left} level={level}/>
                         </div>
@@ -64,14 +105,29 @@ const ChildrenNode = (attributes) => {
                         </div>
                     </div>
                 );
-            } else {
+            } else if(level == 3) {
                 return (
-                    <div style={{display:"none"}} className="Node-item-2">
+                    <div id={id_parent} className="Node-item-2">
                         <div>
                             <Node data={placement.left} level={level}/>
+                            <button id={'button-'+placement.left.distributor_id}  onClick={() => {show_hide_1(placement.left.distributor_id)}}>+</button>
                         </div>
                         <div>
                             <Node data={placement.right} level={level}/>
+                            <button id={'button-'+placement.right.distributor_id}  onClick={() => {show_hide_1(placement.right.distributor_id)}}>+</button>
+                        </div>
+                    </div>
+                );
+            } else {
+                return (
+                    <div id={id_parent} style={{display:"none"}} className="Node-item-2">
+                        <div>
+                            <Node data={placement.left} level={level}/>
+                            <button id={'button-'+placement.left.distributor_id}  onClick={() => {show_hide_1(placement.left.distributor_id)}}>+</button>
+                        </div>
+                        <div>
+                            <Node data={placement.right} level={level}/>
+                            <button id={'button-'+placement.right.distributor_id}  onClick={() => {show_hide_1(placement.right.distributor_id)}}>+</button>
                         </div>
                     </div>
                 );
